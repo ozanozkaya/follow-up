2e7397d7cae187b5d381f8f4001c320375c617ee
ozan.ozky67@hotmail.com <ozan.ozky67@hotmail.com>
2023-11-01 16:46:40 +0300

    Filtering and Diversification

--- src/components/IndexDisplay.jsx
+++ src/components/IndexDisplay.jsx
@@ -1,22 +1,27 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 import "./IndexDisplay.css"; // Import the CSS file
 
 function IndexDisplay() {
   const [data, setData] = useState([]);
+  const [rfusData, setRfusData] = useState([]);
   const [filteredData, setFilteredData] = useState([]);
   const [month, setMonth] = useState("");
+  const [startDate, setStartDate] = useState(null);
+  const [endDate, setEndDate] = useState(null);
   const [search, setSearch] = useState("");
+
+  const applyPresetFilter = (preset) => {
+    setSearch(preset);
+  };
 
   const fetchData = async () => {
     try {
       const response = await axios.get("../Imc.Json");
-      const sectionData = response.data["IMC Hatay Rehabilitation Ass..."];
-      if (Array.isArray(sectionData)) {
-        setData(sectionData);
-      } else {
-        console.error("Specific section data is not an array:", sectionData);
-      }
+      setData(response.data["IMC Hatay Rehabilitation Ass..."]);
+      setRfusData(response.data["RFuS"]);
     } catch (error) {
       console.error("Error fetching the JSON data:", error);
     }
@@ -27,7 +32,13 @@ function IndexDisplay() {
   }, []);
 
   useEffect(() => {
-    let newData = data;
+    let newData = data.map((item) => {
+      const rfusItem = rfusData.find((r) => r._index === item._index);
+      return {
+        ...item,
+        service: rfusItem ? rfusItem["Who did get the service?"] : null,
+      };
+    });
 
     if (month) {
       newData = newData.filter(
@@ -35,36 +46,84 @@ function IndexDisplay() {
       );
     }
 
+    if (startDate || endDate) {
+      newData = newData.filter((item) => {
+        const itemDate = new Date(item.start);
+        return (
+          (!startDate || itemDate >= startDate) &&
+          (!endDate || itemDate <= endDate)
+        );
+      });
+    }
+
     if (search) {
-      newData = newData.filter((item) =>
-        Object.values(item).some((val) =>
-          String(val).toLowerCase().includes(search.toLowerCase())
-        )
+      newData = newData.filter(
+        (item) =>
+          Object.values(item).some((val) =>
+            String(val).toLowerCase().includes(search.toLowerCase())
+          ) ||
+          (item.service &&
+            item.service.toLowerCase().includes(search.toLowerCase()))
       );
     }
 
     setFilteredData(newData);
-  }, [month, search, data]);
+  }, [month, search, startDate, endDate, data, rfusData]);
 
   return (
     <div>
       <div className="filters">
         <select value={month} onChange={(e) => setMonth(e.target.value)}>
           <option value="">Select Month</option>
+          <option value="0">January</option>
+          <option value="1">February</option>
+          <option value="2">March</option>
+          <option value="3">April</option>
+          <option value="4">May</option>
+          <option value="5">June</option>
+          <option value="6">July</option>
+          <option value="7">August</option>
           <option value="8">September</option>
           <option value="9">October</option>
-          {/* Add more months as options here */}
+          <option value="10">November</option>
+          <option value="11">December</option>
         </select>
+
+        <DatePicker
+          selected={startDate}
+          onChange={(date) => setStartDate(date)}
+          selectsStart
+          startDate={startDate}
+          endDate={endDate}
+        />
+
+        <DatePicker
+          selected={endDate}
+          onChange={(date) => setEndDate(date)}
+          selectsEnd
+          startDate={startDate}
+          endDate={endDate}
+        />
 
         <input
           type="text"
           placeholder="Search..."
           value={search}
           onChange={(e) => setSearch(e.target.value)}
-          className="filter-input"
         />
+        <button
+          className="preset-button"
+          onClick={() => applyPresetFilter("Altınözü")}
+        >
+          Altınözü
+        </button>
+        <button
+          className="preset-button"
+          onClick={() => applyPresetFilter("Yayladağı")}
+        >
+          Yayladağı
+        </button>
       </div>
-
       <div className="results-summary">
         <p>Displaying {filteredData.length} results</p>
       </div>
@@ -74,12 +133,12 @@ function IndexDisplay() {
           <div className="card" key={index}>
             <h3>Client: {item["Name of Client"]}</h3>
             <p>
-              Individual Reference Code: {item["Individual Referrence Code"]}
+              Individual Reference Code: {item["Individual Reference Code"]}
             </p>
             <p>Location of the Activity: {item["Location of the Activity"]}</p>
             <p>Date: {new Date(item.start).toLocaleDateString()}</p>
             <p>District: {item["District of Hatay"] || "Not Available"}</p>
-            {/* Add any additional information you'd like to display */}
+            <p>Service: {item.service || "Not Available"}</p>
           </div>
         ))}
       </div>
